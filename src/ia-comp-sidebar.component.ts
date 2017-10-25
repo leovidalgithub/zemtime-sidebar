e27cbf4c007e5fe5c67da7b8da59ad9e7c2f06d0
Leonardo <leo@leovidal.es>
2017-10-25 10:30:21 +0200

    25-oct-2017

--- src/ia-comp-sidebar.component.ts
+++ src/ia-comp-sidebar.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'ia-comp-sidebar',
   templateUrl: './ia-comp-sidebar.component.html',
   styleUrls: ['./ia-comp-sidebar.component.scss']
@@ -16,7 +17,7 @@ export class IaCompSidebarComponent implements OnInit {
   constructor() {
     // Set menu opened default to false
     this.opened = false;
-    this.image = require('../img/zemsania_cut.png');
+    // this.image = require('../img/zemsania_cut.png');
   }
 
   // Toggle opened value
@@ -31,9 +32,10 @@ export class IaCompSidebarComponent implements OnInit {
   // Check if there is a window resize. When it is wider than media query $tablet(768px) turn opened to false
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    let windowSize: number = event.target.innerWidth;
-    if (windowSize > 768)
+    const windowSize: number = event.target.innerWidth;
+    if (windowSize > 768) {
       this.opened = false;
+    }
   }
 
   ngOnInit() {
